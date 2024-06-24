74d5c442a99eea359e193c69b01579d69c6f2bc5
Devante <ddominicci50@gmail.com>
2024-06-23 22:50:03 -0500

    2.7 completed

---
+++ week2/devdom5222_PA22-Home_Laptop.py
@@ -0,0 +1,19 @@
+import random
+
+# Initialize an empty list
+num_list = []
+
+# Append 100 random numbers to the list
+for i in range(100):
+    num_list.append(random.randint(1, 100))
+
+# Print the original list of numbers
+print("Original List:")
+print(num_list)
+
+# Sort the list
+num_list.sort()
+
+# Print the sorted list of numbers
+print("\nSorted List:")
+print(num_list)
